4b65373dfe945dfab0cd8716257a0499d19df937
s-haider10 <s.alihaider543@gmail.com>
2024-10-12 19:42:13 +0800

    Update projectList.tsx

--- app/components/projectList.tsx
+++ app/components/projectList.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { formatDate, getProjectPosts } from "app/project/utils";
+import { formatDate, getProjectPosts } from "app/projects/utils";
 
 export function ProjectPosts() {
   let allBlogs = getProjectPosts();
@@ -19,7 +19,7 @@ export function ProjectPosts() {
           <Link
             key={post.slug}
             className="flex flex-col space-y-1 mb-4"
-            href={`/project/${post.slug}`}
+            href={`/projects/${post.slug}`}
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
               <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
