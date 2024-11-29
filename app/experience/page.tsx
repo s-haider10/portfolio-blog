73c2a9a0aa0a8e5ff0f5bcecda572a35494a3acd
s-haider10 <s.alihaider543@gmail.com>
2024-11-29 23:02:19 +0800

    fix

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -4,7 +4,6 @@ export default function ExperiencePage() {
   return (
     <div>
       <ExperienceList />
-      <div className="fixed inset-0 -z-10 bg-gradient-to-br from-indigo-50 via-white to-cyan-50" />
     </div>
   );
 }
