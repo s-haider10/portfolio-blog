5fd970c525bf73db9e4b9f9918a17b364737e399
s-haider10 <s.alihaider543@gmail.com>
2024-11-30 00:23:47 +0800

    yup

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -3,6 +3,9 @@ import ExperienceList from "app/components/experience";
 export default function ExperiencePage() {
   return (
     <div>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">
+        My Projects
+      </h1>
       <ExperienceList />
     </div>
   );
