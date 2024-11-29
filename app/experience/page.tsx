15f61da6b8a8324c68849f0dfcc43c2e96998351
s-haider10 <s.alihaider543@gmail.com>
2024-11-30 02:38:32 +0800

    data

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -7,26 +7,95 @@ export default function ExperiencePage() {
         Experiences
       </h1>
       <ExperienceList />
-      <h3 className="font-semibold text-xl">Internships</h3>
+
+      {/* New Subsections with Styled Boxes */}
       <div className="experience-subsections">
         <div className="experience-box">
-          <h3 className="font-semibold text-xl">Honors</h3>
-          <p>Research Assistant Fund</p>
-          <p>Dean Undergraduate Research Fund 2024</p>
-          <p>Dean's List for Academic Year, 2022-23</p>
-          <p>Dean's List for Academic Year, 2021-22</p>
+          <h2 className="font-semibold text-xl">Honors & Awards</h2>
+          <div className="awards">
+            <p>
+              <strong>Most Popular Project Award</strong> $|${" "}
+              <em>NYU Shanghai Undergraduate Research Symposium</em>{" "}
+              <span className="float-right">Fall '24</span>
+            </p>
+            <p>
+              <strong>Dean's Undergraduate Research Fund ($1000)</strong> $|${" "}
+              <em>New York University</em>{" "}
+              <span className="float-right">Summer '24</span>
+            </p>
+            <p>
+              <strong>Dean’s List for Academic Year</strong> $|${" "}
+              <em>New York University</em>{" "}
+              <span className="float-right">'21 -- '23</span>
+            </p>
+            <p>
+              <strong>Research Assistant Honorary Grant ($1000)</strong> $|${" "}
+              <em>New York University</em>{" "}
+              <span className="float-right">Spring '23</span>
+            </p>
+            <p>
+              <strong>
+                Scholar’s Full Ride Award, Yale Young Global Scholars ($6500)
+              </strong>{" "}
+              $|$ <em>Yale University</em>{" "}
+              <span className="float-right">Summer '19</span>
+            </p>
+          </div>
         </div>
 
         <div className="experience-box">
-          <h3 className="font-semibold text-xl">Teaching</h3>
-          <p>Teaching Assistant for Data Structures</p>
-          <p>Teaching Assistant for Intro to Computer and Data Science</p>
-          <p>Teaching Assistant for Intro to Computer Programming</p>
+          <h2 className="font-semibold text-xl">Leadership</h2>
+          <div className="leadership">
+            <p>
+              <strong>Tech Lead Dev Team</strong> $|${" "}
+              <em>Fintech & Blockchain Club at NYU</em>{" "}
+              <span className="float-right">Jan '24 -- May '24</span>
+            </p>
+            <p>
+              <strong>Tech Lead & Mentor</strong> $|$ <em>Tech@nyu Club</em>{" "}
+              <span className="float-right">Aug '23 -- Dec '23</span>
+            </p>
+            <p>
+              <strong>Director Tech Consultancy</strong> $|${" "}
+              <em>TAMID Group at NYU</em>{" "}
+              <span className="float-right">Jan '23 -- May '23</span>
+            </p>
+            <p>
+              <strong>Secretary General</strong> $|${" "}
+              <em>New York University Model United Nations VI</em>{" "}
+              <span className="float-right">Jan '23</span>
+            </p>
+          </div>
         </div>
 
         <div className="experience-box">
-          <h3 className="font-semibold text-xl">Leadership</h3>
-          <p>President of the Coding Club</p>
+          <h2 className="font-semibold text-xl">Tutoring</h2>
+          <div className="tutoring">
+            <p>
+              <strong>Lead Course Assistant in CS Discipline</strong> $|${" "}
+              <em>NYU</em>{" "}
+              <span className="float-right">Aug '24 -- Present</span>
+            </p>
+            <p>
+              <strong>Course Assistant for Data Structures</strong> $|${" "}
+              <em>NYU</em>{" "}
+              <span className="float-right">Aug '24 -- Present</span>
+            </p>
+            <p>
+              <strong>
+                Course Assistant for Introduction to Data and Computer Science
+              </strong>{" "}
+              $|$ <em>NYU</em>{" "}
+              <span className="float-right">Jan '23 -- May '23</span>
+            </p>
+            <p>
+              <strong>
+                Course Assistant for Introduction to Computer Programming
+              </strong>{" "}
+              $|$ <em>NYU</em>{" "}
+              <span className="float-right">Aug '22 -- Dec '22</span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
